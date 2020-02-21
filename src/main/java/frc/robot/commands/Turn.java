b2444ef069a5eba66e272c55a267c5ff57d6b72d
Nicholas Xie <nicholasxie@Nicholass-MacBook-Air-2.local>
2020-02-21 16:58:46 -0500

    Co-authored-by: Elijah Kin <ekin@MaggieWalkerHS.onmicrosoft.com>

--- src/main/java/frc/robot/commands/Turn.java
+++ src/main/java/frc/robot/commands/Turn.java
@@ -1,8 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Subsystems;
+
 
 public class Turn extends Command {
 
@@ -10,7 +10,11 @@ public class Turn extends Command {
     private double speed;
     private double timeout;
     private boolean isCorrecting = false;
-
+    private double P = 1;
+    private double I = 1;
+    private double D = 1;
+    private double integral, derivative, previousError = 0;
+    private double PID;
 
     public Turn(double Degrees, double Speed, double Timeout){
         super("Turn");
@@ -26,19 +30,31 @@ public class Turn extends Command {
         Subsystems.driveBase.zeroEncoderPosition();
     }
 
+    public double PID(){
+        double error = degrees - Subsystems.driveBase.getGyroAngle(); // Error = Target - Actual
+        this.integral += (error*.02); // Integral is increased by the error*time (which is .02 seconds using normal IterativeRobot)
+        derivative = (error - this.previousError) / .02;
+        this.previousError = error;
+        this.PID = P * error + I * this.integral + D * derivative;
+        return PID / degrees;
+    }
+
     public void execute() {
         if ((degrees > 0) && !isCorrecting) {
             // Turning to the right
-            Subsystems.driveBase.setMotors(-speed, speed);
+            Subsystems.driveBase.setMotors(-speed * PID(), speed * PID());
         } else if ((degrees < 0) && !isCorrecting) {
             // Turning to the left
-            Subsystems.driveBase.setMotors(speed, -speed);
+            PID();
+            Subsystems.driveBase.setMotors(speed * PID(), -speed * PID());
         } else if (degrees > 0) {
             // Turned to the right, but correcting to the left
-            Subsystems.driveBase.setMotors(speed / 2, -speed / 2);
+            PID();
+            Subsystems.driveBase.setMotors(speed / 2 * PID(), -speed / 2 * PID());
         } else {
             // Turned to the left, but correcting to the right
-            Subsystems.driveBase.setMotors(-speed / 2, speed / 2);
+            PID();
+            Subsystems.driveBase.setMotors(-speed / 2 * PID(), speed / 2 * PID());
         }
     }
 
